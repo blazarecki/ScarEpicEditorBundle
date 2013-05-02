65825a00c1ab80c2a8dd69be2ff547ffd9ef7d66
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2013-05-03 00:03:42 +0200

    Don't use short open tag

--- Resources/views/Form/epic_editor_widget.html.php
+++ Resources/views/Form/epic_editor_widget.html.php
@@ -1,31 +1,31 @@
-<textarea style="display: none;" <?= $view['form']->block($form, 'attributes'); ?>></textarea>
+<textarea style="display: none;" <?php echo $view['form']->block($form, 'attributes'); ?>></textarea>
 <div id="epic_editor"></div>
 
-<script type="text/javascript" src="<?= $js_path; ?>"></script>
+<script type="text/javascript" src="<?php echo $js_path; ?>"></script>
 <script type="text/javascript">
 (function() {
     var opts = {
-        'container'         : '<?= $options['container']; ?>',
-        'basePath'          : '<?= $options['base_path']; ?>',
-        'clientSideStorage' : '<?= $options['client_side_storage']; ?>',
-        'localStorageName'  : '<?= $options['local_storage_name']; ?>',
-        'parser'            : <?= $options['parser']; ?>,
-        'focusOnLoad'       : '<?= $options['focus_on_load']; ?>',
+        'container'         : '<?php echo $options['container']; ?>',
+        'basePath'          : '<?php echo $options['base_path']; ?>',
+        'clientSideStorage' : '<?php echo $options['client_side_storage']; ?>',
+        'localStorageName'  : '<?php echo $options['local_storage_name']; ?>',
+        'parser'            : <?php echo $options['parser']; ?>,
+        'focusOnLoad'       : '<?php echo $options['focus_on_load']; ?>',
         'file' : {
-            'name'           : '<?= $options['file']['name']; ?>',
-            'defaultContent' : '<?= $options['file']['default_content']; ?>',
-            'autoSave'       : '<?= $options['file']['auto_save']; ?>'
+            'name'           : '<?php echo $options['file']['name']; ?>',
+            'defaultContent' : '<?php echo $options['file']['default_content']; ?>',
+            'autoSave'       : '<?php echo $options['file']['auto_save']; ?>'
         },
         'theme' : {
-            'base'    : '<?= $options['theme']['base']; ?>',
-            'preview' : '<?= $options['theme']['preview']; ?>',
-            'editor'  : '<?= $options['theme']['editor']; ?>'
+            'base'    : '<?php echo $options['theme']['base']; ?>',
+            'preview' : '<?php echo $options['theme']['preview']; ?>',
+            'editor'  : '<?php echo $options['theme']['editor']; ?>'
         },
         'shortcut' : {
-            'modifier'   : '<?= $options['shortcut']['modifier']; ?>',
-            'fullscreen' : '<?= $options['shortcut']['fullscreen']; ?>',
-            'preview'    : '<?= $options['shortcut']['preview']; ?>',
-            'edit'       : '<?= $options['shortcut']['edit']; ?>'
+            'modifier'   : '<?php echo $options['shortcut']['modifier']; ?>',
+            'fullscreen' : '<?php echo $options['shortcut']['fullscreen']; ?>',
+            'preview'    : '<?php echo $options['shortcut']['preview']; ?>',
+            'edit'       : '<?php echo $options['shortcut']['edit']; ?>'
         }
     };
 
